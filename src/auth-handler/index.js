9a3a7d76d998f74e28a50dc144c6d6f370382418
Jaypee <jignacio@cambridge.org>
2023-07-26 18:46:07 +0800

    Add a working lambda auth

--- src/auth-handler/index.js
+++ src/auth-handler/index.js
@@ -1,9 +1,12 @@
-const jwt = require('jsonwebtoken');
-const jwksClient = require('jwks-rsa');
+const axios = require('axios');
+const jsonwebtoken = require('jsonwebtoken');
+const NodeRSA = require('node-rsa');
 
-const client = jwksClient({
-  jwksUri: 'https://accounts.eu1.gigya.com/accounts.getJWTPublicKey',
-});
+const webKeyUrl = process.env.JWKS_URL || 'https://accounts.eu1.gigya.com/accounts.getJWTPublicKey';
+
+const deserializeKey  = (key) => key
+  .replace(/-/g, '+')
+  .replace(/_/g, '/');
 
 const responsePolicy = (effect, routeArn, context) => {
   return {
@@ -21,26 +24,32 @@ const responsePolicy = (effect, routeArn, context) => {
 }
 
 const handler = async (event) => {
+  const jwks = await axios.get(webKeyUrl);
+  const publicKey = jwks.data;
+  const modulus = Buffer.from(deserializeKey(publicKey.n), 'base64')
+  const exponent = (typeof publicKey.e === 'number')
+      ? publicKey.e
+      : Buffer.from(publicKey.e, 'base64')
+  
+  const pubKey = new NodeRSA()
+      pubKey.importKey({
+        e: exponent,
+        n: modulus
+      }, 'components-public')
+  const pubPem = pubKey
+        .exportKey('pkcs1-public')
+        .toString('utf8')
+
   try {
-    const token = event.headers.authorization.split(' ')[1];
-    
-    if(token === 'salaam') {
-      return responsePolicy("Allow", event.routeArn, {bypassJwt: true})
-    }
+    const jws = jsonwebtoken
+      .verify(idToken, pubPem, { algorithms: ['RS256'] })
 
-    const decoded = jwt.decode(token, { complete: true });
-    const kid = decoded.header.kid;
-  
-    const key = await client.getSigningKeyAsync(kid);
-    const signingKey = key.getPublicKey();
-  
-    const context = jwt.verify(token, signingKey);
-
-    return responsePolicy("Allow", event.routeArn, context)
+    return responsePolicy("Allow", event.routeArn, jws);
   } catch (error) {
     console.error(JSON.stringify({ message: error.message }));
   }
-  return responsePolicy("Deny", event.routeArn, {})
+
+  return responsePolicy("Deny", event.routeArn, {});
 }
 
 module.exports = { handler };
