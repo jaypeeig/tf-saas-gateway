f6a5c4bdc78f8f06169ded164af18c06c08dc3b9
Jaypee <jignacio@cambridge.org>
2023-07-26 14:12:34 +0800

    Add jwt sapcdc verifier

--- src/auth-handler/index.js
+++ src/auth-handler/index.js
@@ -1,46 +1,46 @@
-const handler = async (event) => {
-  console.log('Evt: ' + event.headers.authorization)
-  if (event.headers.authorization === "Bearer salaam") {
-    console.log("allowed");
-    return {
-      principalId: "abcdef", // The principal user identification associated with the token sent by the client.
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [{
-          Action: "execute-api:Invoke",
-          Effect: "Allow",
-          Resource: event.routeArn
-        }]
-      },
-      context: {
-        stringKey: "value",
-        numberKey: 1,
-        booleanKey: true,
-        arrayKey: ["value1", "value2"],
-        mapKey: { value1: "value2" }
-      }
-    };
-  } else {
-    console.log("denied");
-    return {
-      principalId: "abcdef", // The principal user identification associated with the token sent by the client.
-      policyDocument: {
-        Version: "2012-10-17",
-        Statement: [{
-          Action: "execute-api:Invoke",
-          Effect: "Deny",
-          Resource: event.routeArn
-        }]
-      },
-      context: {
-        stringKey: "value",
-        numberKey: 1,
-        booleanKey: true,
-        arrayKey: ["value1", "value2"],
-        mapKey: { value1: "value2" }
-      }
-    };
+const jwt = require('jsonwebtoken');
+const jwksClient = require('jwks-rsa');
+
+const client = jwksClient({
+  jwksUri: 'https://accounts.eu1.gigya.com/accounts.getJWTPublicKey',
+});
+
+const responsePolicy = (effect, routeArn, context) => {
+  return {
+    principalId: "foobar",
+    policyDocument: {
+      Version: "2012-10-17",
+      Statement: [{
+        Action: "execute-api:Invoke",
+        Effect: effect,
+        Resource: routeArn
+      }]
+    },
+    context
   }
-};
+}
+
+const handler = async (event) => {
+  try {
+    const token = event.headers.authorization.split(' ')[1];
+    
+    if(token === 'salaam') {
+      return responsePolicy("Allow", event.routeArn, {bypassJwt: true})
+    }
+
+    const decoded = jwt.decode(token, { complete: true });
+    const kid = decoded.header.kid;
+  
+    const key = await client.getSigningKeyAsync(kid);
+    const signingKey = key.getPublicKey();
+  
+    const context = jwt.verify(token, signingKey);
+
+    return responsePolicy("Allow", event.routeArn, context)
+  } catch (error) {
+    console.error(JSON.stringify({ message: error.message }));
+  }
+  return responsePolicy("Deny", event.routeArn, {})
+}
 
 module.exports = { handler };
